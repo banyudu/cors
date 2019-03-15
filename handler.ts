f57794049b9fc83bc1b762e67ba30b90ad9ef363
Yudu Ban <banyudu@innobuddy.com>
2019-03-15 18:19:17 +0800

    implement

--- handler.ts
+++ handler.ts
@@ -1,4 +1,21 @@
+import axios from 'axios'
 
-export const get = (event: any, context: any, callback: any) => {
-  //
+export const get = async (event: any, context: any, callback: any) => {
+  const parameters = Object.keys(event.queryStringParameters)
+  if (parameters.length !== 1) {
+    callback(null, {
+      statusCode: 400
+    })
+  }
+  const url = parameters[0]
+  try {
+    const result = await axios.get(url)
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(result.data)
+    })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
